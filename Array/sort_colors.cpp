46f84c39299de2b95830b0a968224c584583c83b
Varshithsingampalli <98725136+Varshithsingampalli@users.noreply.github.com>
2024-07-17 21:00:01 +0530

    Create sort_colors.cpp

---
+++ Array/sort_colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int lo=0,mid=0,hi=nums.size()-1;
+        while(mid<=hi)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[mid],nums[lo]);
+                lo++;
+                mid++;
+            }
+            else if(nums[mid]==2)
+            {
+                swap(nums[mid],nums[hi]);
+                hi--;
+            }
+           else mid++;
+        }
+    }
+};
